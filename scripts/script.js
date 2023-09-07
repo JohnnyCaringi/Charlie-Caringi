604a55cc01d9cf8fdd3c40de0a06ba0e4196255c
JohnnyCaringi <92838873+JohnnyCaringi@users.noreply.github.com>
2023-09-07 14:51:23 -0500

    Add files via upload
    -The HTML now bolds the text "The Doge™" and italicizes instructions to add some flair
    -The CSS now centers all of the text to make the experience feel more special
    -The Javascript now adds a fire background after the sunglasses are put on The Doge for maximum cool factor

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
   
   $("#memeSunglasses").draggable({ stop: function(){
       if($("#memeSunglasses").position().left > 380 && $("#memeSunglasses").position().left < 450  && $("#memeSunglasses").position().top > 95 && $("#memeSunglasses").position().top < 125){
-      $("#wowText").text("WOW! The Doge™ is looking pretty cool!");
+        $("#wowText").text("WOW! The Doge™ is looking pretty cool!");
+        document.body.style.backgroundImage = "url(https://e0.pxfuel.com/wallpapers/542/56/desktop-wallpaper-fire-background-group-76-throughout.jpg)";
     }
   }
    });
